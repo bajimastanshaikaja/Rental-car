d206c4fa44caeb801994c89a764e1e94540d0c4c
bajimastanshaikaja <s.mastan@ajacs.in>
2026-04-18 12:29:31 +0530

    added cn library and changed manage and browse cars

--- Rental_car/src/admin/components/AdminNavbar.jsx
+++ Rental_car/src/admin/components/AdminNavbar.jsx
@@ -9,18 +9,18 @@ export const AdminNavbar = () => {
     return (
         <div className='shadow-lg rounded-4xl sticky-0  max-w-5/6' >
             <BrowserRouter>
-            <ul className='flex gap-5 border border-none
+                <ul className='flex gap-5 border border-none
              rounded-2xl  '>
-                <li><Link to="/dashboard">dashboard</Link></li>
-                <li><Link to="/managecars">ManageCars</Link></li>
-                <li><Link to="/bookings">ManageBookings</Link></li>
-            </ul>
-            <Routes>
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                <Route path="/managecars" element={<ManageCars/>}/>
-                <Route path="/bookings" element={<ManageBookings/>}/>
-                <Route path="/addcar" element={<Addcars/>}/>
-            </Routes>
+                    <li><Link to="/dashboard">dashboard</Link></li>
+                    <li><Link to="/managecars">ManageCars</Link></li>
+                    <li><Link to="/bookings">ManageBookings</Link></li>
+                </ul>
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/managecars" element={<ManageCars />} />
+                    <Route path="/bookings" element={<ManageBookings />} />
+
+                </Routes>
             </BrowserRouter>
         </div>
     )
