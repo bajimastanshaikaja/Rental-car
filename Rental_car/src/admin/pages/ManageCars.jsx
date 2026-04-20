1e0051c5ea617eea25f9c1d6b04c5d0a4244a381
DevasenaMedavaram360 <de.medavaram@ajacs.in>
2026-04-20 12:29:14 +0530

    pulled changes

--- Rental_car/src/admin/pages/ManageCars.jsx
+++ Rental_car/src/admin/pages/ManageCars.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export const ManageCars = () => {
-  
-    return (
-        <div>
-            <div>
-                <h1>Manage Cars</h1>
-                
-            </div>
+  return (
+    <div>
+      <div className="flex justify-between mx-5">
+        <div className="ml-3">
+          <h1>Manage Cars</h1>
         </div>
-    )
-}
+        <div className="mr-3">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+            Add Cars
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
